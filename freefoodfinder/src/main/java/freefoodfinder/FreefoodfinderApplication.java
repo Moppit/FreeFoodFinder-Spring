f3ec66f47d3ccb93d7dc145d2a9b3d3c08934883
Moppit <michelletranusa@gmail.com>
2021-11-12 18:46:41 -0700

    Generated Spring Project

---
+++ freefoodfinder/src/main/java/freefoodfinder/FreefoodfinderApplication.java
@@ -0,0 +1,13 @@
+package freefoodfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreefoodfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreefoodfinderApplication.class, args);
+	}
+
+}
---
+++ freefoodfinder/src/test/java/freefoodfinder/FreefoodfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package freefoodfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreefoodfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
