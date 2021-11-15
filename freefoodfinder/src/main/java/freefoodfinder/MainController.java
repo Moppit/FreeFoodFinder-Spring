a0ac04e8e1cd525c8518483a1a141b797ef96c80
Moppit <michelletranusa@gmail.com>
2021-11-15 16:43:12 -0700

    Enabled cross origin requests

--- freefoodfinder/src/main/java/freefoodfinder/MainController.java
+++ freefoodfinder/src/main/java/freefoodfinder/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @RequestMapping("/fff")
@@ -13,6 +14,7 @@ public class MainController {
     @Autowired
     private MainService service;
 
+    @CrossOrigin
     @GetMapping("/events")
     public @ResponseBody EventResponse getAllEvents() {
         return this.service.getAllEvents();
