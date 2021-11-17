bdca9ff2b9c26b805487cfcb6e5c17fcd0f6a600
Jett Crowson <jecr4062@colorado.edu>
2021-11-16 21:59:52 -0700

    Change locations in mock data to be central

--- db/Food_Finding_Mock_Data.sql
+++ db/Food_Finding_Mock_Data.sql
@@ -20,8 +20,8 @@ select * from dietary_restriction;
 describe event;
 INSERT INTO event 
 VALUES
-	(1,"cookies", "2021-11-18 15:30:00", "Freshman Welcome!", "ECCE 188", 1, 1),
-	(2,"pizza", "2021-11-18 16:30:09", "Bold center event", "BOLD Center" ,2, 2),
-    (3,"bagel", "2021-11-17 12:30:00", "this is a food description", NULL, 3, 3);
+	(DEFAULT,"Free Cookies!", "2021-11-18 15:30:00", "Freshman Welcome!", "ECCE 188", 1, 101),
+	(DEFAULT,"Leftover Pizza + Soda", "2021-11-18 16:30:09", "Bold center event", "BOLD Center" ,2, 76),
+    (DEFAULT,"bagel", "2021-11-17 12:30:00", "this is a food description", NULL, 3, 105);
 select * from event;
 
